c8d63164d6ede8385de1ded4ea14d183c54003c8
Austin Amaya <austin.amaya@gmail.com>
2014-12-21 18:13:38 -0500

    solution to homework assignment
    Solution to the homework assignment. Function creates a matrix object
    that caches its own inverse.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,31 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  xInv <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    xInv <<- NULL
+  }
+  
+  get <- function() {
+    x
+  }
+  
+  setinv <- function(inverse) {
+    xInv <<- inverse
+  }
+  
+  getinv <- function() {
+    xInv
+  }
+  
+  list(
+    set = set, 
+    get = get, 
+    setinv = setinv, 
+    getinv = getinv
+  )
 }
 
 
@@ -12,4 +36,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  xInv <- x$getinv()
+  
+  ## check whether we already have the inverse
+  if(!is.null(xInv)) {
+    message("getting cached data")
+    return(xInv)
+  }
+  
+  ## no luck, we'll have to compute it ourselves
+  data <- x$get()
+  xInv <- solve(data,...)
+  x$setinv(xInv)
+  return(xInv)
 }
